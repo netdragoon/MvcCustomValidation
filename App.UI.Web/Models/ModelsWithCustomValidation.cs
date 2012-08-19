fd0a245a55c1e3300eaf6c406ffbb09bee2c8fd5
Pawel Nowosielski <pnowosielski@pnowosie-pc.local>
2012-08-19 21:30:41 +0200

    Get advance of DataAnnotations.DataTypeAttribute

--- App.UI.Web/Models/ModelsWithCustomValidation.cs
+++ App.UI.Web/Models/ModelsWithCustomValidation.cs
@@ -2,25 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using FB.MVC;
 using FB.MVC.Validation;
+using DataAnnotations = System.ComponentModel.DataAnnotations;
 
 namespace App.UI.Web.Models
 {
     public class ChangePasswordModel
     {
         [Required]
-        //[DataType(DataType.Password)]
+        [DataAnnotations.DataType(DataAnnotations.DataType.Password)]
         [Display(Name = "Current password")]
         public string OldPassword { get; set; }
 
         [Required]
         [StringLength(100, MinimumLength = 6)] //ErrorMessage = "The {0} must be at least {2} characters long.", 
-        //[DataType(DataType.Password)]
+        [DataAnnotations.DataType(DataAnnotations.DataType.Password)]
         [Display(Name = "New password")]
         public string NewPassword { get; set; }
 
-        //[DataType(DataType.Password)]
+        [DataAnnotations.DataType(DataAnnotations.DataType.Password)]
         [Display(Name = "Confirm new password")]
         [System.Web.Mvc.Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
@@ -33,7 +33,7 @@ namespace App.UI.Web.Models
         public string UserName { get; set; }
 
         [Required]
-        //[DataType(DataType.Password)]
+        [DataAnnotations.DataType(DataAnnotations.DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
 
@@ -48,17 +48,17 @@ namespace App.UI.Web.Models
         public string UserName { get; set; }
 
         [Required]
-        //[DataType(DataType.EmailAddress)]
+        [DataAnnotations.DataType(DataAnnotations.DataType.EmailAddress)]
         [Display(Name = "Email address")]
         public string Email { get; set; }
 
         [Required]
         [StringLength(100, MinimumLength = 6)]
-        //[DataType(DataType.Password)]
+        [DataAnnotations.DataType(DataAnnotations.DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
 
-        //[DataType(DataType.Password)]
+        [DataAnnotations.DataType(DataAnnotations.DataType.Password)]
         [Display(Name = "Confirm password")]
         [System.Web.Mvc.Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
